2ea67d5e91769ed7dc68e115175b5d8ba0f6132b
aemc <aemlcruz@gmail.com>
2018-03-29 10:52:09 -0400

    Fix spacing

--- blockchain.py
+++ blockchain.py
@@ -3,19 +3,19 @@ class Blockchain(object):
         self.chain = []
         self.current_transactions = []
 
-        def new_block(self):
-            # Creates a new Block and adds it to the chain
-            pass
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
 
-        def new_transaction(self):
-            pass
+    def new_transaction(self):
+        pass
 
-        @staticmethod
-        def hash(block):
-            # Hashes a Block
-            pass
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
 
-        @property
-        def last_block(self):
-            # Returns the last Block in the chain
-            pass
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
